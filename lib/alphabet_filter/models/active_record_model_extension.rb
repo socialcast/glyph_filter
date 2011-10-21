c371d9309d14f280dc2aa44e4b85bc8436de733c
Sean Cashin <scashin133@gmail.com>
2011-10-20 22:25:43 -0700

    Removing unused declaration

--- lib/alphabet_filter/models/active_record_model_extension.rb
+++ lib/alphabet_filter/models/active_record_model_extension.rb
@@ -3,11 +3,9 @@ module AlphabetFilter
     extend ActiveSupport::Concern
 
     included do
-      self.send(:include, AlphabetFilter::ConfigurationMethods)
-
       self.scope :letter_filter, Proc.new {|column, filter_letter|
         return where({}) if filter_letter.blank?
-        return where("#{table_name}.#{column} regexp '^[^#{letters.join("|")}|#{letters.map(&:downcase).join("|")}]'") if filter_letter == punctuation
+        return where("#{table_name}.#{column} regexp '^[^#{AlphabetFilter.config.letters.join("|")}|#{AlphabetFilter.config.letters.map(&:downcase).join("|")}]'") if filter_letter == AlphabetFilter.config.punctuation
         where("#{table_name}.#{column} like ?", filter_letter + "%")
       }
       
