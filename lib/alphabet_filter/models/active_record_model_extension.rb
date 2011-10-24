3647c7e40a2600464733ed30e02fdbc302a60146
Sean Cashin <scashin133@gmail.com>
2011-10-23 20:17:49 -0700

    Cant use a return in a Proc

--- lib/alphabet_filter/models/active_record_model_extension.rb
+++ lib/alphabet_filter/models/active_record_model_extension.rb
@@ -4,9 +4,13 @@ module AlphabetFilter
 
     included do
       self.scope :letter_filter, Proc.new {|column, filter_letter|
-        return where({}) if filter_letter.blank?
-        return where("#{table_name}.#{column} regexp '^[^#{AlphabetFilter.config.letters.join("|")}|#{AlphabetFilter.config.letters.map(&:downcase).join("|")}]'") if filter_letter == AlphabetFilter.config.punctuation
-        where("#{table_name}.#{column} like ?", filter_letter + "%")
+        if filter_letter.blank?
+          where({})
+        elsif filter_letter == AlphabetFilter.config.punctuation
+          where("#{table_name}.#{column} regexp '^[^#{AlphabetFilter.config.letters.join("|")}|#{AlphabetFilter.config.letters.map(&:downcase).join("|")}]'")
+        else
+          where("#{table_name}.#{column} like ?", filter_letter + "%")
+        end
       }
       
     end
