32ca7db0adae2d5ec077e352414677929e3481a4
Sean Cashin <scashin133@gmail.com>
2012-02-28 14:49:57 -0800

    Removing old Test::Unit tests.

--- test/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '7d20af8b5f500b559c975e71e327aa09a5f4d8558dbc1cbe2f3c2f223f8590d1856698541d27d95545ff5a31a74f57ca63f463172e54c5f8767a819da1db16e6'
